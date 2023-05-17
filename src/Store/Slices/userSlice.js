12825a15598a8b7b7459e385dccf464f7d633ece
pablo2637 <pabli2637@gmail.com>
2023-05-17 19:50:07 +0200

    map v1

--- src/Store/Slices/userSlice.js
+++ src/Store/Slices/userSlice.js
@@ -1,32 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createSlice } from '@reduxjs/toolkit'
 
 export const userSlice = createSlice({
 
     name: 'user',
+
     initialState: {
+
         user: {},
+        status: 'non-authenticated',
         errorMessage: undefined
     },
-    reducers: {
-        onLogin: (state, action) => {
 
+    reducers: {
+
+        onLogin: (state, action) => {
+            
             state.user = action.payload,
-                state.errorMessage = {}
+            state.errorMessage = {},
+            state.status = 'authenticated'
         },
-        onLogout: (state, action) => {
+
+        onLogout: (state,action) => {
+
             state.user = {};
             state.errorMessage = action.payload;
+            state.status = 'non-authenticated'
         },
-        onRegister: (state, action) => {
+
+        onRegister: (state,action) => {
+
             state.user = action.payload;
             state.errorMessage = {}
         },
-        onError: (state, action) => {
+
+        onError: (state,action) => {
+
             console.log('esto es action', action)
             state.user = {};
             state.errorMessage = action.payload
         }
     }
 })
+
 export const { onLogin, onLogout, onRegister, onError } = userSlice.actions;
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from './Slices/userSlice';
 
-
 export const store = configureStore({
-  reducer: {
-    user: userSlice.reducer
 
-  }
-});
+    reducer: {
+
+      user: userSlice.reducer
+        
+    }
+
+  });

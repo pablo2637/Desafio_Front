db6e903e89e6e72a50150b796cd92b0b8dae2bf1
pablo2637 <pabli2637@gmail.com>
2023-05-18 11:42:41 +0200

    map con distancia ok

--- src/Public/Components/UserMarker.jsx
+++ src/Public/Components/UserMarker.jsx
@@ -1,28 +1,39 @@
 import { useEffect, useState } from 'react';
 import { Marker, Popup, useMapEvents } from 'react-leaflet';
+import { Icon, Tooltip } from 'leaflet';
+import { useDispatch, useSelector } from 'react-redux';
+import { onLoadCoords } from '../../Store/Slices/userSlice'
 
 
 export const UserMarker = () => {
 
     const [position, setPosition] = useState(null);
+    const dispatch = useDispatch();
+
+    const customIcon = new Icon({
+        iconUrl: "../assets/person.png",
+        iconSize: [45, 45]
+    })
+
 
     const map = useMapEvents({
-        
+
         locationfound(e) {
             setPosition(e.latlng)
             map.flyTo(e.latlng, map.getZoom())
+            dispatch(onLoadCoords({ lat: e.latitude, long: e.longitude }))
         },
     })
-    
+
 
     useEffect(() => {
         map.locate();
-      }, [map]);
+    }, [map]);
 
     return position === null ? null : (
 
-        <Marker position={position}>
-            <Popup>You are here</Popup>
+        <Marker position={position} icon={customIcon}>
+            <Popup>Estas aquí</Popup>
         </Marker>
     );
 };

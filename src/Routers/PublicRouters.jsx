28703cbc428a2962afa404d42847a2012ec02c2e
Develokev <develokev@gmail.com>
2023-05-18 11:43:49 +0200

    Login form operative, error operative, console check.

--- src/Routers/PublicRouters.jsx
+++ src/Routers/PublicRouters.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Home } from '../Public/Pages';
 import { RegisterPage } from '../Auth/Pages';
+import { LoginPage } from '../Auth/Pages/LoginPage';
 
 
 export const PublicRoutes = () => {
@@ -17,6 +18,11 @@ export const PublicRoutes = () => {
                 <Route
                     path='register'
                     element={<RegisterPage />}
+                />
+
+                <Route
+                    path='login'
+                    element={<LoginPage />}
                 />
 
                 <Route path='/*' element={<Navigate to={'/'} />} />
--- src/Store/Slices/userSlice.js
+++ src/Store/Slices/userSlice.js
@@ -14,6 +14,8 @@ export const userSlice = createSlice({
     reducers: {
 
         onLogin: (state, action) => {
+
+            console.log('esto es action', action)
             
             state.user = action.payload,
             state.errorMessage = {},
@@ -30,12 +32,12 @@ export const userSlice = createSlice({
         onRegister: (state,action) => {
 
             state.user = action.payload;
-            state.errorMessage = {}
+            state.errorMessage = {},
+            state.status = 'authenticated'
         },
 
         onError: (state,action) => {
 
-            console.log('esto es action', action)
             state.user = {};
             state.errorMessage = action.payload
         }

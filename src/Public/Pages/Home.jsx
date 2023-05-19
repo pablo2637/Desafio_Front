15cef2e45cc266c8d3db5252f971dacd79faf165
mariaal21 <malmazandelpie@gmail.com>
2023-05-19 13:21:37 +0200

    NavBar con authenticad en caso de no estar logeado

--- src/Public/Pages/Home.jsx
+++ src/Public/Pages/Home.jsx
@@ -1,13 +1,12 @@
 import { Map } from "../Components";
-import { CreateQR } from "../Components/CreateQR";
+
 
 export const Home = () => {
 
     return (
         <>
         <Map />
-
-        <CreateQR/>
+       
         </>
     );
 };

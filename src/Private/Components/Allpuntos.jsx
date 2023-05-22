196d48a7fa7772912df6a7f60c03df86af6af3c1
pablo2637 <pabli2637@gmail.com>
2023-05-22 20:30:28 +0200

    carousel

--- src/Private/Components/Allpuntos.jsx
+++ src/Private/Components/Allpuntos.jsx
@@ -1,6 +1,12 @@
 
+import { useNavigate } from 'react-router-dom';
+
 
 export const Allpuntos = ({ sums }) => {
+
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate('/impacto');
 
   return (
     <div className="flex justify-around">
@@ -12,13 +18,14 @@ export const Allpuntos = ({ sums }) => {
         <p className='col-span-full text-base text-center pb-1 font-medium'>Puntos acumulados</p>
       </div>
 
-      <button className="w-40 mb-0 rounded-lg bg-slate-50 gridPoints grid justify-center text-left items-center">
+      <button onClick={handleClick} className="w-40 mb-0 rounded-lg bg-slate-50 gridPoints grid justify-center text-left items-center">
         <div className='h-4/5 grid justify-end'>
           <img className='w-auto h-full object-contain' src="../assets/impact.png" />
         </div>
         <p className="text-2xl font-bold">{sums.liters} l.</p>
         <p className='col-span-full text-base text-center pb-1 font-medium'>Impacto en el agura</p>
       </button>
+
     </div>
   )
 
--- src/Private/Components/CarrouselAgua.jsx
+++ src/Private/Components/CarrouselAgua.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-
-import Aceite from '../../../public/assets/Aceite.png'
+import { Carousel } from 'react-responsive-carousel';
 import '../../Main.css'
-
-
 
 
 export const CarrouselAgua = () => {
   return (
     <>
-    
-    <h2>Tu balance - Tu impacto en el agua</h2>
 
-    <div>
-    <h2 className="absolute top-2/3 mt-12 left-1/2 transform -translate-x-1/2 -translate-y-3/2 text-center font-semibold text-orange-500">
-    Gracias por reciclar Aceite
-    </h2>
-    <img src={Aceite} alt="rectángulo" className="mancha mx-auto" />
-    </div>
+      <h2>Tu balance - Tu impacto en el agua</h2>
 
-    
+
+      <div className='Container-carr'>
+
+        <p className='texto'>Aprende a usar nuestra Web</p>
+
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showThumbs={false}
+          showIndicators
+          centerMode={false}
+          className="carousel-container">
+
+          <div>
+            <h2 className="absolute top-2/3 mt-12 left-1/2 transform -translate-x-1/2 -translate-y-3/2 text-center font-semibold text-orange-500">
+              Gracias por reciclar Aceite
+            </h2>
+            <img src={'../assets/Aceite.png'} alt="rectángulo" className="mancha mx-auto" />
+          </div>
+
+          <div>
+            <h2 className="absolute top-2/3 mt-12 left-1/2 transform -translate-x-1/2 -translate-y-3/2 text-center font-semibold text-orange-500">
+              Gracias por reciclar Aceite
+            </h2>
+            <img src={'../assets/Aceite.png'} alt="rectángulo" className="mancha mx-auto" />
+          </div>
+
+          <div>
+            <h2 className="absolute top-2/3 mt-12 left-1/2 transform -translate-x-1/2 -translate-y-3/2 text-center font-semibold text-orange-500">
+              Gracias por reciclar Aceite
+            </h2>
+            <img src={'../assets/Aceite.png'} alt="rectángulo" className="mancha mx-auto" />
+          </div>
+
+
+        </Carousel>
+
+      </div>
 
 
     </>
 
-    
+
   )
 }

46b5dd9c834bd6132d4918a368d4e3a38f0e711a
Develokev <develokev@gmail.com>
2023-05-22 20:10:11 +0200

    restaurant Question Toggle operative, missing to bring state to complete texts on cards

---
+++ src/Private/Components/FavRestaurant.jsx
@@ -0,0 +1,11 @@
+
+export const FavRestaurant = () => {
+
+
+  return (
+
+    <>
+        
+    </>
+  )
+}
--- src/Private/Components/PointsObtained.jsx
+++ src/Private/Components/PointsObtained.jsx
@@ -1,19 +1,32 @@
-import {NavLink} from 'react-router-dom'
+import { useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 
 export const PointsObtained = () => {
+
+    const [screenOne, setScreenOne] = useState(true)
+
+    const navigate = useNavigate()
+
+    const handleToggle = () => setScreenOne(!screenOne)
+
+    const handleAnswer = () => {
+
+        setScreenOne(screenOne);
+        navigate('/mispuntos')
+    }
 
 
   return (
 
-
-    <>
-        <article className="gcardContainer">
+    <>  {
+            screenOne ? 
+            <article className="gcardContainer">
 
             <div className="gCard">
 
                 <h3>Has ganado {} puntos!</h3> 
                 
-                <p>Con {} de aceite has ganado {} puntos y has logrado salvar {} litros de agua. 
+                <p>Con {}L de aceite has ganado {} puntos y has logrado salvar {} litros de agua. 
                                         Muy bien, ¡ sigue así!</p>
 
                 <img 
@@ -22,16 +35,57 @@ export const PointsObtained = () => {
                 className='mt-4' />
 
                 <div className="centerContainer">
-                    <NavLink 
-                    to=""
-                    className="group rounded h-8 w-11/12 bg-amber-600 text-base text-white hover:bg-amber-500 my-4">
+                    <button 
+                    onClick={handleToggle}
+                    className=" rounded h-8 w-11/12 bg-amber-600 text-base text-white hover:bg-amber-500 my-4">
                         Continuar
-
-                    </NavLink>
+                    </button>
                 </div>
             </div>
 
         </article>
+        :
+        <article className="gcardContainer">
+
+            <div className="gCard">
+
+                <h3>Ahora que has reciclado</h3> 
+                
+                <p>¿Gastarías tus {} puntos en {}?</p>
+
+                <img 
+                // src={}
+                alt="Restaurant Picture"
+                className='mt-4 rounded p-4' />
+
+                <button>
+                    <img
+                    onClick={handleAnswer}
+                    className='mx-7 mt-3'
+                    src="\assets\noRed.png" 
+                    alt="noIcon" />
+                </button>
+
+                <button>
+                    <img 
+                    onClick={handleAnswer}
+                    className='mx-7 mt-3'
+                    src="\assets\yesBlue.png" 
+                    alt="yesIcon" />
+                </button>
+
+                <button
+                onClick={handleAnswer}
+                className="group rounded h-8 w-11/12 bg-amber-600 text-base text-white hover:bg-amber-500 my-4">
+                    
+                    Saltar
+                    
+                </button>
+            </div>
+
+        </article>
+    }
+        
     </>
   )
 }
